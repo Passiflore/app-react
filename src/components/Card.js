6b0a4c2e8b83e26c490f73f2c4a93bc1c25597c3
Eneyran <60504924+Eneyran@users.noreply.github.com>
2021-03-25 16:34:01 +0100

    Card overlay
    Making card overlay

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Card = (props) => {
     const {movie} = props;
     return (
-        <a className="movieCard" href="#movieCardPopup">
+        <a className="movieCard" href="#moviePopUp">
             <div className="moviePosterPlace">
 
                 <div class="moviePosterOverlay"></div>
@@ -12,18 +12,29 @@ const Card = (props) => {
                 <img className="moviePoster" src={"https://image.tmdb.org/t/p/w200"+movie.poster_path} alt = "movie poster"></img>
             
 		        <div class="moviePosterOverlayText">
-                    <a href="#see-more">Voir plus</a>
+                    <a className="SeeMore" href="#moviePopUp">Voir plus</a>
                 </div>
 	        </div>
+
             <p className="movieTitle">{movie.title}</p>
-            <ul className="movieInformations">
-                <li>Note: {movie.rating}</li>
-                <li>Titre original: {movie.original_title}</li>
-                <li>Date de sortie: {movie.release_date}</li>
-                <li>Synopsis: {movie.overview}</li>
-            </ul>
+
+            <div id="moviePopUp" class="overlay">
+                <div class="popup">
+                    <h2 className="movieTitle">{movie.title}</h2>
+                    <a class="close" href="#">&times;</a>
+                    <div class="movieInformations">
+                        <div>Titre original: {movie.original_title}</div>
+                        <div>Note: {movie.rating}</div>
+                    
+                        <div>Date de sortie: {movie.release_date}</div>
+                        <div>Synopsis: {movie.overview}</div>
+                    </div>
+                </div>
+            </div>
+
         </a>
     );
 };
 
 export default Card;
+
